9d17d013cf2fe0a16b54a0a4e81eb41352f82d65
emsharp <emsharp@ncsu.edu>
2016-01-15 14:28:38 -0500

    Added Skeleton Classes for Current Scripts
    Does not include deprecated or experimental scripts

--- NutraNinja/Assets/MyScripts/CollisionManager.cs
+++ NutraNinja/Assets/MyScripts/CollisionManager.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Windows.Kinect;
 
+/// <summary>
+/// Increments or decrements the player's score based
+/// on whether the player has sliced a healthy/unhealthy
+/// GameObject
+/// @author Emily Sharp
+/// </summary>
 public class CollisionManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public int goodFoodScoreScheme = 10;
+	public int badFoodScoreScheme = -10;
+
+	// Keep track of number of objects player has slashed
+	// Accessible to other classes
+	public static int objectsSlashed;
+
+	void Start()
+	{
+		objectsSlashed = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	// Hadnl
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "GoodFood") 
+		{
+			objectsSlashed += 1;
+			ScoreManager.currentScore += goodFoodScoreScheme;
+			Destroy(col.gameObject);
+		}
+		if (col.gameObject.tag == "BadFood")
+		{
+			ScoreManager.currentScore += badFoodScoreScheme;
+			Destroy(col.gameObject);
+		}
 	}
 }
